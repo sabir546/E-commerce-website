4462811e87a197d8805a1b76654213a7b6ec19b7
sabir546 <mdgulamsabir9@gmail.com>
2024-03-16 03:38:56 +0530

    Upto Order summary page compleated

---
+++ src/customer/component/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AddressCard = () => {
+  return (
+    <div>
+      <div className="space-y-3">
+        <p className="font-semibold">raam kapoor</p>
+        <p>Bhopal ashokagarder,462023</p>
+        <div className="space-y-1">
+          <p className="font-semibold">Phone Number</p>
+          <p>234565433 </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddressCard;
--- src/customer/component/Check-out/CheckOut.jsx
+++ src/customer/component/Check-out/CheckOut.jsx
@@ -5,34 +5,20 @@ import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-
-const steps = [
-  "Select campaign settings",
-  "Create an ad group",
-  "Create an ad",
-];
+import { useLocation } from "react-router-dom";
+import AddDeliveryForm from "./AddDeliveryForm";
+import OrderSummery from "./OrderSummery";
+const steps = ["Login", " Delivery address", "Order summery", "Payments"];
 
 export default function CheckOut() {
   const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-
-//   const isStepOptional = (step) => {
-//     return step === 1;
-//   };
-
-//   const isStepSkipped = (step) => {
-//     return skipped.has(step);
-//   };
+  // const [skipped, setSkipped] = React.useState(new Set());
+  const location = useLocation();
+  const querySearch = new URLSearchParams(location.search);
+  const step = querySearch.get("step");
 
   const handleNext = () => {
-    // let newSkipped = skipped;
-    // if (isStepSkipped(activeStep)) {
-    //   newSkipped = new Set(newSkipped.values());
-    //   newSkipped.delete(activeStep);
-    // }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    // setSkipped(newSkipped);
   };
 
   const handleBack = () => {
@@ -40,55 +26,44 @@ export default function CheckOut() {
   };
 
   return (
-    <div>
+    <div className="px-10 lg:px-20 ">
+      <Box sx={{ width: "100%" }}>
+        <Stepper activeStep={step}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
 
-    <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-        //   if (isStepOptional(index)) {
-        //     labelProps.optional = (
-        //       <Typography variant="caption">Optional</Typography>
-        //     );
-        //   }
-        //   if (isStepSkipped(index)) {
-        //     stepProps.completed = false;
-        //   }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: "1 1 auto" }} />
-
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? "Finish" : "Next"}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
-    </Box>
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+            </Box>
+            <div className="mt-10">
+              {step == 2 ? <AddDeliveryForm /> : <OrderSummery />}
+            </div>
+          </React.Fragment>
+        )}
+      </Box>
     </div>
   );
 }
